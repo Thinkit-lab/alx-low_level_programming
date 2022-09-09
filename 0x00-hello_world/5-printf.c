04f90f13171c9adbc5594e81c2e9d9f81b5a10d4
Thinkit-lab <afolabikunle2@gmail.com>
2022-09-08 17:40:26 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,11 @@
 /**
  * main - Entry point
  * description - using the main function
+ * printf - prints to standard output
  * return - 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 }
