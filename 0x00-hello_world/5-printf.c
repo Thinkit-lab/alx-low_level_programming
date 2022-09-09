c574afada7885e34186ee6e35537f7f26e340a01
Thinkit-lab <afolabikunle2@gmail.com>
2022-09-08 17:32:47 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * description - using the main function
+ * return - 0
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return(0);
+}
