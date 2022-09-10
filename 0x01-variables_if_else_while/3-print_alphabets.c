4e8f3e3a0e6d302d697aca3de24f235bb3692892
Thinkit-lab <afolabikunle2@gmail.com>
2022-09-10 06:50:11 -0700

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 
 /**
- *  * main - Entry point
- *   * Return: 0
- *    */
+ * main - Entry point
+ *  Return: 0
+ */
 int main(void)
 {
-	 char ch;
-
-	 for (ch = 'a'; ch <= 'z'; ch++)
-	 {
-		 putchar(ch);
-	 }
-	 for (ch = 'A'; ch <= 'Z'; ch++)
-	 {
-		 putchar(ch);
-	 }
-	 putchar('\n');
-	 return (0);
+       	char ch;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	       	putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+	       	putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
