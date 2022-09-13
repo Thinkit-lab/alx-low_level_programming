a4b3c0359e3d6dba4d8c93c612a6ed3e0fff4d2a
Thinkit-lab <afolabikunle2@gmail.com>
2022-09-13 12:57:36 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,7 +4,7 @@
 /**
  * _abs - function that computes the absolute value of an integer
  * @c: is the int that will be used for the argument of the function
- * Result: 0
+ * Return: 0
  */
 int _abs(int c)
 {
